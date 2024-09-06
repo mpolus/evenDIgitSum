690c72f3b650f3b609021806c7ac3357f4991298
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-09-06 13:23:41 -0400

    sum

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+
+    private static int getEvenDigitSum(int number) {
+        if (number < 0){
+            return -1;
+        }
+
+        int sum = 0;
+        int lastDigit = 0;
+        while (number > 0){
+            lastDigit = number % 10;
+            if (lastDigit  % 2 == 0){
+                sum += lastDigit;
+            }
+
+
+            number /= 10;
+
+
+        }
+        return sum;
+
+    }
 }
